143ca013947941de93fa41ced89add82e73464a4
Shandler Rechenberger <83612169+ShandlerR@users.noreply.github.com>
2024-01-10 12:04:48 -0700

    Cleaned out for new Semester

--- scripts/getdates.js
+++
@@ -1,8 +0,0 @@
-const lastMod = document.getElementById("lastModified");
-const year = document.getElementById("year");
-
-let lastModDate = new Date(document.lastModified);
-let currentDate = new Date().getFullYear();
-
-lastMod.innerHTML = lastModDate;
-year.innerHTML = currentDate;
