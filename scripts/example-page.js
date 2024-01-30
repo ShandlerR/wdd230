df9ee1cc751b3cbaff26185f55b906df204210d6
Shandler Rechenberger <83612169+ShandlerR@users.noreply.github.com>
2024-01-30 07:45:49 -0700

    Added Error Comment

--- scripts/example-page.js
+++ scripts/example-page.js
@@ -4,5 +4,6 @@ function onClickEvent()
 {
     number ++;
     console.log(number);
-    console.warn("Something Spooky Happens here!")
+    console.warn("Something Spooky Happens here!");
+    //console.error("SELF DESTRUCT!!!");
 }
