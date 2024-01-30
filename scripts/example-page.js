b118d9477d3b04701d63773f7f76be5a51a6fd1a
Shandler Rechenberger <83612169+ShandlerR@users.noreply.github.com>
2024-01-30 07:53:08 -0700

    Finished Example Page

--- scripts/example-page.js
+++ scripts/example-page.js
@@ -4,6 +4,6 @@ function onClickEvent()
 {
     number ++;
     console.log(number);
-    console.warn("Something Spooky Happens here!");
+    //console.warn("Something Spooky Happens here!");
     //console.error("SELF DESTRUCT!!!");
 }
