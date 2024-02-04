b6daa2e1d509f3639e202df9d145d7bccf3ebbc6
Shandler Rechenberger <83612169+ShandlerR@users.noreply.github.com>
2024-02-04 10:31:27 -0700

    Overhauled Small View

--- chamber/scripts/menu.js
+++ chamber/scripts/menu.js
@@ -1,22 +1,17 @@
 const hamButton = document.querySelector("#menu");
 
-const navigation = document.querySelector("ul");
+const navMenu = document.querySelector("nav");
 
-const background = document.querySelector("#box");
+const openNavCommand = "navMenuOpen"
 
 hamButton.addEventListener
 (
     "click", () => 
     {
-        console.log("HELLO WORLD!");
-        // The menu is opened by default, we need to add a way to close it.
 
-        // Adds or removes the "closed" Tag to the nav class
-        navigation.classList.toggle("closed"); 
 
-        // Adds or removes the "Closed" Tag to the button
-        hamButton.classList.toggle("closed");
+        navMenu.classList.toggle(openNavCommand);
 
-        background.classList.toggle("closed");
+        hamButton.classList.toggle(openNavCommand);
     }
 );
