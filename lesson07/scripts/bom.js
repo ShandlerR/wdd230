3004a044e39c59ff7a1319c93670344d3b9166f7
Shandler Rechenberger <83612169+ShandlerR@users.noreply.github.com>
2024-02-22 09:39:07 -0700

    k

---
+++ lesson07/scripts/bom.js
@@ -0,0 +1,63 @@
+const input = document.querySelector("input");
+const button = document.querySelector("button");
+const ul = document.querySelector("#list");
+
+
+button.addEventListener
+(
+    "click", function() 
+    {
+        if(input.value != "")
+        {
+            displayList(input.value);
+            chapterArray.append(input.value);
+            setChapterList();
+            input.value = "";
+            input.focus();
+        }
+    }
+)
+
+function displayList(item) 
+{
+    const li = document.createElement("li");
+    li.textContent = item;
+    
+    const deleteButton = document.createElement("button");
+    deleteButton.textContent = "❌";
+    deleteButton.addEventListener
+    (
+        "click", function()
+        {
+            item = "";
+            input.focus();
+            deleteButton.parentElement.remove();
+        }
+    )
+
+    li.append(deleteButton);
+    ul.append(li);
+}
+
+function setChapterList()
+{
+    localStorage.setItem("favoriteBOM", JSON.stringify(chapterArray));
+}
+
+function getChapterList()
+{
+    return JSON.parse(localStorage.getItem("favoriteBOM"))
+}
+
+var chapterArray = getChapterList() || [];
+
+chapterArray.forEach(chapter => {
+    displayList(chapter);
+});
+
+function deleteChapter(chapter)
+{
+    chapter = chapter.slice(0, chapter.length - 1); 
+    chapterArray = chapterArray.filter((item) => item !== chapter);
+    setChapterList();
+}
