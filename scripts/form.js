de9eeb86352d7221b8d1f1677e217d329ceb37ed
Shandler Rechenberger <83612169+ShandlerR@users.noreply.github.com>
2024-03-02 11:41:18 -0700

    ad

---
+++ scripts/form.js
@@ -0,0 +1,10 @@
+const pw1 = document.querySelector("#password");
+const pw2 = document.querySelector("#rPassword");
+const message = "";
+
+pw2.addEventListener("focusout", checkSame)
+
+function checkSame() 
+{
+    console.log("I can't read");
+}
