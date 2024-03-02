265f3f7d6e691f8fe5bff180483940fdccbff945
Shandler Rechenberger <83612169+ShandlerR@users.noreply.github.com>
2024-03-02 12:16:11 -0700

    Finished Form's HTML content

--- scripts/form.js
+++ scripts/form.js
@@ -1,8 +1,18 @@
 const pw1 = document.querySelector("#password");
 const pw2 = document.querySelector("#rPassword");
 const message = document.querySelector("#formMessage");
+const rangeValue = document.querySelector("#rangeValue");
+const ratingSlider = document.querySelector("#rating");
 
-pw2.addEventListener("focusout", checkSame)
+pw2.addEventListener("focusout", checkSame);
+ratingSlider.addEventListener("change", displayRating);
+ratingSlider.addEventListener("input", displayRating);
+
+
+function displayRating()
+{
+    rangeValue.innerHTML = ratingSlider.value;   
+}
 
 function checkSame() 
 {
