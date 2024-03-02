8499abe5ecb0e4d87db5f56f94bb09f8492f9ec2
Shandler Rechenberger <83612169+ShandlerR@users.noreply.github.com>
2024-03-02 11:57:58 -0700

    Password Works!

--- scripts/form.js
+++ scripts/form.js
@@ -1,10 +1,24 @@
 const pw1 = document.querySelector("#password");
 const pw2 = document.querySelector("#rPassword");
-const message = "";
+const message = document.querySelector("#formMessage");
 
 pw2.addEventListener("focusout", checkSame)
 
 function checkSame() 
 {
-    console.log("I can't read");
+    if (pw1.value !== pw2.value)
+    {
+        message.textContent = "Passwords do not match";
+		message.style.visibility = "show";
+		pw2.style.backgroundColor = "#fff0f3";
+		
+        pw2.value = "";
+		pw2.focus();
+	}
+    else 
+    {
+        message.textContent = "";
+        pw2.style.backgroundColor = "white";
+        console.log("Same");
+    }
 }
