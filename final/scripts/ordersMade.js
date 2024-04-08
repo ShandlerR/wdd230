b3c7c4a46baae2beaafcd4e3c367372ccf4030db
Shandler Rechenberger <83612169+ShandlerR@users.noreply.github.com>
2024-04-08 13:06:25 -0600

    About us Finished

---
+++ final/scripts/ordersMade.js
@@ -0,0 +1,18 @@
+function main()
+{
+    let ordersMade = localStorage.getItem("ordersMade");
+    const ordersElement = document.querySelector("#orders");
+
+    // console.log(ordersMade);
+
+    if(ordersMade === null)
+    {
+        ordersMade = 0;
+        localStorage.setItem("ordersMade", "0")
+    }
+
+    ordersElement.textContent = ordersMade;
+
+}
+
+main();
